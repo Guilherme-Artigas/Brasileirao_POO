ed41d5acc2d4c56a66e2ee954ac68faa5702e5f2
Guilherme Artigas <guilherme.artigas92@gmail.com>
2023-03-06 16:40:45 -0300

    feat: endpoint /leaderboard/home retorna informações de desempenho dos times ordenado por resultados.

--- app/backend/src/services/LeaderBoardService.ts
+++ app/backend/src/services/LeaderBoardService.ts
@@ -4,33 +4,124 @@ import IPerformance from '../interfaces/IPerformance';
 
 import MatchModel from '../database/models/MatchModel';
 import TeamModel from '../database/models/TeamModel';
-import IMatch from '../interfaces/IMatch';
-import ITeam from '../interfaces/TeamInterface';
 
 export default class LeaderBoardService implements IlbService {
   protected lbModel: ModelStatic<MatchModel>;
   protected teamModel: ModelStatic<TeamModel>;
+  private _countGames: number;
+  private _totalPoints: number;
+  private _totalVictories: number;
+  private _totalDraws: number;
+  private _totalLosses: number;
+  private _goalsFavor: number;
+  private _goalsOwn: number;
 
   constructor() {
     this.lbModel = MatchModel;
     this.teamModel = TeamModel;
+    this._countGames = 0;
+    this._totalPoints = 0;
+    this._totalVictories = 0;
+    this._totalDraws = 0;
+    this._totalLosses = 0;
+    this._goalsFavor = 0;
+    this._goalsOwn = 0;
   }
 
-  async initialPerformance(): Promise<IPerformance[] | IMatch[] | ITeam[]> {
-    const allTeams = await this.teamModel.findAll();
-    const allPerformance = allTeams.map((team) => {
-      const obj = {
-        name: team.teamName,
-        totalPoints: 0,
-        totalGames: 0,
-        totalVictories: 0,
-        totalDraws: 0,
-        totalLosses: 0,
-        goalsFavor: 0,
-        goalsOwn: 0,
-      };
-      return obj;
+  totalGames(id: number, allMatches: MatchModel[]): number {
+    this._countGames = 0;
+    allMatches.forEach((match) => {
+      if (match.homeTeamId === id) this._countGames += 1;
     });
+    return this._countGames;
+  }
+
+  totalPoints(id: number, allMatches: MatchModel[]): number {
+    this._totalPoints = 0;
+    allMatches.forEach((match) => {
+      if (match.homeTeamId === id && match.homeTeamGoals > match.awayTeamGoals) {
+        this._totalPoints += 3;
+      }
+      if (match.homeTeamId === id && match.homeTeamGoals === match.awayTeamGoals) {
+        this._totalPoints += 1;
+      }
+    });
+    return this._totalPoints;
+  }
+
+  totalVictories(id: number, allMatches: MatchModel[]): number {
+    this._totalVictories = 0;
+    allMatches.forEach((match) => {
+      if (match.homeTeamId === id && match.homeTeamGoals > match.awayTeamGoals) {
+        this._totalVictories += 1;
+      }
+    });
+    return this._totalVictories;
+  }
+
+  totalDraws(id: number, allMatches: MatchModel[]): number {
+    this._totalDraws = 0;
+    allMatches.forEach((match) => {
+      if (match.homeTeamId === id && match.homeTeamGoals === match.awayTeamGoals) {
+        this._totalDraws += 1;
+      }
+    });
+    return this._totalDraws;
+  }
+
+  totalLosses(id: number, allMatches: MatchModel[]): number {
+    this._totalLosses = 0;
+    allMatches.forEach((match) => {
+      if (match.homeTeamId === id && match.homeTeamGoals < match.awayTeamGoals) {
+        this._totalLosses += 1;
+      }
+    });
+    return this._totalLosses;
+  }
+
+  goalsFavor(id: number, allMatches: MatchModel[]): number {
+    this._goalsFavor = 0;
+    allMatches.forEach((match) => {
+      if (match.homeTeamId === id) this._goalsFavor += match.homeTeamGoals;
+    });
+    return this._goalsFavor;
+  }
+
+  goalsOwn(id: number, allMatches: MatchModel[]): number {
+    this._goalsOwn = 0;
+    allMatches.forEach((match) => {
+      if (match.homeTeamId === id) this._goalsOwn += match.awayTeamGoals;
+    });
+    return this._goalsOwn;
+  }
+
+  static tiebreaker(list: IPerformance[]): IPerformance[] {
+    list.sort((a, b) =>
+      b.totalPoints - a.totalPoints
+      || b.totalVictories - a.totalVictories
+      || b.goalsBalance - a.goalsBalance
+      || b.goalsFavor - a.goalsFavor
+      || b.goalsOwn - a.goalsOwn);
+    return list;
+  }
+
+  async initialPerformance(): Promise<IPerformance[]> {
+    const allMatches = await this.lbModel.findAll({ where: { inProgress: false } });
+    const allTeams = await this.teamModel.findAll();
+    const allPerformance = allTeams.map((team) => ({
+      name: team.teamName,
+      totalPoints: this.totalPoints(team.id, allMatches),
+      totalGames: this.totalGames(team.id, allMatches),
+      totalVictories: this.totalVictories(team.id, allMatches),
+      totalDraws: this.totalDraws(team.id, allMatches),
+      totalLosses: this.totalLosses(team.id, allMatches),
+      goalsFavor: this.goalsFavor(team.id, allMatches),
+      goalsOwn: this.goalsOwn(team.id, allMatches),
+      goalsBalance: this.goalsFavor(team.id, allMatches) - this.goalsOwn(team.id, allMatches),
+      efficiency: ((this.totalPoints(team.id, allMatches)
+        / (this.totalGames(team.id, allMatches) * 3)) * 100).toFixed(2),
+    }));
+    LeaderBoardService.tiebreaker(allPerformance);
     return allPerformance;
   }
 }
--- app/backend/src/services/interfaces/LbServiceInterface.ts
+++ app/backend/src/services/interfaces/LbServiceInterface.ts
@@ -1,7 +1,5 @@
 import IPerformance from '../../interfaces/IPerformance';
-import IMatch from '../../interfaces/IMatch';
-import ITeam from '../../interfaces/TeamInterface';
 
 export default interface IlbService {
-  initialPerformance(): Promise<IPerformance[] | IMatch[] | ITeam[]>;
+  initialPerformance(): Promise<IPerformance[]>;
 }
